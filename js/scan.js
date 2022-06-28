985cfe816fbd5884c47608310517cf7691bc6e18
trung hieu <trunghieudinh224@gmail.com>
2022-06-28 10:53:42 +0700

    báºarcode scan

--- js/scan.js
+++ js/scan.js
@@ -26,33 +26,47 @@
 //     console.log(data.codeResult.code);
 // }); 
 
-const barcodeValue = document.getElementById("barcodeValuee") 
 
-        Quagga.init({
-            inputStream: {
-                name: "Live",
-                type: "LiveStream",
-                target: document.querySelector('#camera')
-                // area: { // defines rectangle of the detection/localization area
-                //   top: '25%',
-                //   right: '0%',
-                //   left: '0%',
-                //   bottom: '25%'
-                // },
-            },
-            decoder: {
-                readers: ["code_128_reader"]
-            }
-        }, function (err) {
-            if (err) {
-                console.log(err);
-                return
-            }
-            console.log("Initialization finished. Ready to start");
-            Quagga.start();
-        });
 
-        Quagga.onDetected(function (data) {
-            console.log(data.codeResult.code);
-			document.getElementById("barcodeValue").innerHTML = data.codeResult.code;
-        }); 
+const startScan = () => {
+    const button = document.getElementById("camera");
+    button.style.display = "block";
+
+    Quagga.init({
+        inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector('#camera')
+            // area: { // defines rectangle of the detection/localization area
+            //   top: '25%',
+            //   right: '0%',
+            //   left: '0%',
+            //   bottom: '25%'
+            // },
+        },
+        decoder: {
+            readers: [
+                "code_128_reader",
+                'code_39_reader',
+                'code_39_vin_reader',
+                'upc_reader',
+                'upc_e_reader',
+                'codabar_reader',
+                'code_93_reader'
+            ]
+        }
+    }, function (err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+    });
+    
+    Quagga.onDetected(function (data) {
+        console.log(data.codeResult.code);
+        document.getElementById("barcodeValue").value = data.codeResult.code;
+        document.getElementById("barcodeType").value = data.codeResult.format;
+    }); 
+}
