8bf7c04e816e36822c2aa4227decd9c070a6dd34
trung hieu <trunghieudinh224@gmail.com>
2022-07-18 10:29:10 +0700

    test print

--- js/print.js
+++ js/print.js
@@ -73,15 +73,15 @@ var textLayout = ["0", "1", "2"];
 function prepareDataPrint() {
     let str = (document.getElementById("nameForm").innerText).trim() + titleSplit + "1" + titleSplit + "1" + titleSign +
         splitString(document.getElementById("Combobox").options[document.getElementById("Combobox").selectedIndex].innerText.trim()) + textLayout[0] + tabSign + textSize[0] + lineSign +
-        prepareLine(document.getElementById("title1").innerText, document.getElementById("value1").value, document.getElementById("unit1").value) + textLayout[0] + tabSign + textSize[0] + lineSign +
-        prepareLine(document.getElementById("title4").innerText, document.getElementById("value4").value, document.getElementById("unit4").value) + textLayout[0] + tabSign + textSize[0] + lineSign +
-        prepareLine(document.getElementById("title3").innerText, document.getElementById("value3").value, document.getElementById("unit3").value) + textLayout[0] + tabSign + textSize[0] + lineSign +
-        prepareLine(document.getElementById("title5").innerText, document.getElementById("value5").value, document.getElementById("unit5").value) + textLayout[0] + tabSign + textSize[0] + lineSign +
-        prepareLine(document.getElementById("title6").innerText, document.getElementById("value6").value, document.getElementById("unit6").value) + textLayout[0] + tabSign + textSize[0] + lineSign +
-        prepareLine(document.getElementById("title7").innerText, document.getElementById("value7").value, document.getElementById("unit7").value) + textLayout[0] + tabSign + textSize[0] + lineSign +
-        prepareLine(document.getElementById("title2").innerText, document.getElementById("value2").value, document.getElementById("unit2").value) + textLayout[0] + tabSign + textSize[0] + lineSign +
-        prepareLine(document.getElementById("title8").innerText, document.getElementById("value8").value, document.getElementById("unit8").value) + textLayout[0] + tabSign + textSize[0] + lineSign +
-        prepareLine(document.getElementById("title9").innerText, document.getElementById("value9").value, document.getElementById("unit9").value) + textLayout[0] + tabSign + textSize[0];
+        prepareLine(document.getElementById("title1").innerText, document.getElementById("value1").value, document.getElementById("unit1").value) + tabSign + textLayout[0] + tabSign + textSize[0] + lineSign +
+        prepareLine(document.getElementById("title4").innerText, document.getElementById("value4").value, document.getElementById("unit4").value) + tabSign + textLayout[0] + tabSign + textSize[0] + lineSign +
+        prepareLine(document.getElementById("title3").innerText, document.getElementById("value3").value, document.getElementById("unit3").value) + tabSign + textLayout[0] + tabSign + textSize[0] + lineSign +
+        prepareLine(document.getElementById("title5").innerText, document.getElementById("value5").value, document.getElementById("unit5").value) + tabSign + textLayout[0] + tabSign + textSize[0] + lineSign +
+        prepareLine(document.getElementById("title6").innerText, document.getElementById("value6").value, document.getElementById("unit6").value) + tabSign + textLayout[0] + tabSign + textSize[0] + lineSign +
+        prepareLine(document.getElementById("title7").innerText, document.getElementById("value7").value, document.getElementById("unit7").value) + tabSign + textLayout[0] + tabSign + textSize[0] + lineSign +
+        prepareLine(document.getElementById("title2").innerText, document.getElementById("value2").value, document.getElementById("unit2").value) + tabSign + textLayout[0] + tabSign + textSize[0] + lineSign +
+        prepareLine(document.getElementById("title8").innerText, document.getElementById("value8").value, document.getElementById("unit8").value) + tabSign + textLayout[0] + tabSign + textSize[0] + lineSign +
+        prepareLine(document.getElementById("title9").innerText, document.getElementById("value9").value, document.getElementById("unit9").value) + tabSign + textLayout[0] + tabSign + textSize[0];
 
 
     console.log(str);
