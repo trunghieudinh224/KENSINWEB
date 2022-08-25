7148cfc679f0fa3064472ee8146856fa3a135238
trung hieu <trunghieudinh224@gmail.com>
2022-08-25 16:35:52 +0700

    print image

--- js/print.js
+++ js/print.js
@@ -300,7 +300,7 @@ function sendImage() {
 
 var stringImage = "";
 function createImage() {
-    setupFormPrint("100vh", "650px", "60px", "28px", "32px", "28px", "32px");
+    setupFormPrint("100vh", "650px", "60px", "29px", "33px", "29px", "33px");
     domtoimage.toBlob(document.getElementById('shukeiForm'))
     .then(function(blob){
         // window.saveAs(blob, "output.pdf");
