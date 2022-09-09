86ab15eab28b2616c23f0eaab8f3f71b1de0f9bd
trung hieu <trunghieudinh224@gmail.com>
2022-09-09 09:39:49 +0700

    add message

--- js/Constant/message.js
+++ js/Constant/message.js
@@ -1,19 +1,27 @@
 /*
     ******  INFO MESSAGE  ******
 */
-// message 1
+// info 1
 export var I00001 = "message1"
 
 
 /*
     ******  ERROR MESSAGE  ******
 */
-// message 1
-export var E00001 = "message1"
+// 顧客コード正しくない
+export var E00001 = "顧客コード正しくない"
+// 日本語で入力して下さい
+export var E00002 = "日本語で入力して下さい"
+// 電話番号入力してください
+export var E00003 = "電話番号入力してください"
+// メーター機番が間違います。
+export var E00004 = "メーター機番が間違います。"
+// データありません。
+export var E00005 = "データありません。"
 
 
 /*
     ******  WARNING MESSAGE  ******
 */
-// message 1
+// warning 1
 export var W00001 = "message1"
