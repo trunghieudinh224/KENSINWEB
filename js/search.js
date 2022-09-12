6f97a122d28f46dd1cf6749678d76e437e5c4022
trung hieu <trunghieudinh224@gmail.com>
2022-09-12 10:34:30 +0700

    add taishoo data

--- js/search.js
+++ js/search.js
@@ -130,7 +130,7 @@ searchBtn.onclick = function () {
         newElement.appendChild(newStatus);
         table.appendChild(newElement);
         newElement.onclick = function(){
-          item.taishoo = searchOrderText[order];
+          item.taishoo = searchOrder[order];
           const cusdat = Object.assign({}, item)
           localStorage.setItem("cusdat", JSON.stringify(cusdat));
           window.location.href = "/kokyaku_sentaku_page.html";
