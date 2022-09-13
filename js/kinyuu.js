76a67cf79735126dec53d994a544bf6f7b4ad540
huytoan150593 <huytoan150593@gmail.com>
2022-09-13 15:59:51 +0700

    uchiwake added

--- js/kinyuu.js
+++ js/kinyuu.js
@@ -5,10 +5,13 @@ import * as constant from './Constant/message.js'
 const closeBtn = document.querySelector("#close-icon");
 const overlay = document.querySelector(".overlay");
 const detail = document.querySelector("#detail-btn");
+const wrapMainForm = document.querySelector(".overlay .container-mainform .wrap-mainform");
 
 closeBtn.onclick = function(){
-    overlay.classList.remove("overlay-animate");
+    overlay.style.zIndex = "-1";
+    wrapMainForm.classList.remove("wrap-mainform-animate");
 }
 detail.onclick = function(){
-    overlay.classList.add("overlay-animate");
+    overlay.style.zIndex = "2";
+    wrapMainForm.classList.add("wrap-mainform-animate");
 }
