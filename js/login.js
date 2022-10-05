521331487fd4929d3d419793160fcdb7bc728d43
trung hieu <trunghieudinh224@gmail.com>
2022-10-05 14:22:30 +0700

    change js link

--- js/login.js
+++ js/login.js
@@ -1,8 +1,9 @@
+import * as Common from './Common/common_function.js'
 
-const user = document.querySelector("#user");
-const password = document.querySelector("#pass");
-const loginBtn = document.querySelector("#btnLogin");
 
+/* 
+	CHECK ACCOUNT
+*/
 function checkAccount() {
     var data = sessionStorage.getItem('username');
     if (data != null) {
@@ -10,19 +11,35 @@ function checkAccount() {
     }
 }
 
-window.onload = checkAccount;
 
-function validate(user, pass) {
-    if ((!user || !pass) && user !== "" && pass !== "") {
-        setupModal("error", "ログイン", "ユーザIDとパスワードを入力してください", "確認", null);
-        return false;
+/* 
+	LOGIN ACTION
+*/
+function login() {
+    let user = document.getElementById("user"); 
+    let password = document.getElementById("pass"); 
+    if (user.value === "" || password.value === "") {
+        if (user.value === "") {
+            user.classList.add("warning");
+        }
+        if (password.value === "") {
+            password.classList.add("warning");
+        }
+    } else {
+        Common.setupModal("load", null, "しばらくお待ちください。。。", null, null);
+        checkUser(user.value, password.value);
     }
-    return true;
 }
 
+
+/* 
+	CHECK USER ACCOUNT
+    @param username
+    @param password
+*/
 function checkUser(username, password) {
     $.ajax({
-        url: "https://192.168.200.218/Webkensin/compackr/loginchk?key=0582668301&login_id=" + username + "&login_pw=" + password,
+        url: "http://192.168.200.218:8080/Webkensin/compackr/loginchk?key=0582668301&login_id=" + username + "&login_pw=" + password,
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
@@ -32,9 +49,9 @@ function checkUser(username, password) {
             if (JSON.parse(result).err_code == 0) {
                 sessionStorage.setItem('username', username);
                 sessionStorage.setItem('password', password);
-                getInformation();
+                getSystemDat();
             } else {
-                setupModal("error", "ログイン", "ログインに失敗しました", "確認", null);
+                Common.setupModal("error", "ログイン", "ログインに失敗しました", "確認", null);
             }
         },
         error: function (jqXHR, exception) {
@@ -44,84 +61,63 @@ function checkUser(username, password) {
     });
 }
 
-// ----------------Check input value------------------>
-function login() {
-    if (user.value === "" || password.value === "") {
-        if (user.value === "") {
-            user.classList.add("warning");
+
+/* 
+	SET FOCUS INPUT
+*/
+function setFocusInput() {
+    let user = document.getElementById("user"); 
+    let password = document.getElementById("pass"); 
+    user.onfocus = function () {
+        if (user.classList.contains("warning")) {
+            user.classList.remove("warning");
         }
-        if (password.value === "") {
-            password.classList.add("warning");
+    }
+    password.onfocus = function () {
+        if (password.classList.contains("warning")) {
+            password.classList.remove("warning");
         }
-    } else {
-        setupModal("load", null, "しばらくお待ちください。。。", null, null);
-        checkUser(user.value, password.value);
-    }
-}
-user.onfocus = function () {
-    if (user.classList.contains("warning")) {
-        user.classList.remove("warning");
-    }
-}
-password.onfocus = function () {
-    if (password.classList.contains("warning")) {
-        password.classList.remove("warning");
     }
 }
 
 
-function getInformation() {
+/* 
+	GET SYSTEM DATA
+*/
+function getSystemDat() {
     $.ajax({
-        url: "https://192.168.200.218/Webkensin/compackr/readData?key=0582668301&cusrec=0&login_id=7&login_pw=7",
+        url: "http://192.168.200.218:8080/Webkensin/compackr/readData?key=0582668301&cusrec=0&login_id=" + sessionStorage.getItem('username') + "&login_pw=" + sessionStorage.getItem('password'),
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         success: function (result) {
-            systemDat = JSON.parse(result);
+            let systemDat = JSON.parse(result);
             localStorage.setItem("UserData", JSON.stringify(systemDat));
             window.location.href = "/menu_page.html";
         },
         error: function (jqXHR, exception) {
             console.log(exception);
-        }
+        }, 
+		timeout: 20000
     });
 }
 
-function setupModal(status, title, message, textButton1, textButton2) {
-    var modal = document.getElementById("myModal");
-    var imgModal = document.getElementsByClassName("modal-image")[0];
-    var titleModal = document.getElementsByClassName("title-modal")[0];
-    var messageModal = document.getElementsByClassName("modal-message-detail")[0];
-    var buttonConfirm = document.getElementsByClassName("button-confirm")[0];
-    var closeButton = document.getElementsByClassName("modal-close-button")[0];
 
-    titleModal.innerHTML = title;
-    messageModal.innerHTML = message;
-
-    if (status == "load") {
-        imgModal.src = "../images/gif/gif_loading_data.gif";
-        titleModal.style.display = "none";
-        closeButton.style.display = "none";
-        buttonConfirm.style.display = "none";
-    } else {
-        if (status == "info") {
-
-        } else if (status == "error") {
-            imgModal.src = "../images/gif/gif_fail.gif";
-        }
-
-        if (textButton1 != null) {
-            buttonConfirm.style.display = "block";
-        }
-    }
-
-    buttonConfirm.onclick = function () {
-        modal.style.display = "none";
-    }
-    // When the user clicks on <span> (x), close the modal
-    closeButton.onclick = function () {
-        modal.style.display = "none";
-    }
-
-    modal.style.display = "block";
+/* 
+	ONCLICK ACTION
+*/
+function onclickAction() {
+	document.getElementById("loginButton").onclick = login;
 }
+
+
+/* 
+	ONLOAD ACTION
+*/
+function onLoadAction() {
+    checkAccount();
+	onclickAction();
+    setFocusInput();
+}
+
+window.onload = onLoadAction;
