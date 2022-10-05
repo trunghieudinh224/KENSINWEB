2861e2972d13d55d7b1d172f0fd185222ae9226b
trung hieu <trunghieudinh224@gmail.com>
2022-10-05 14:23:03 +0700

    edit logic setting page

--- js/setting.js
+++ js/setting.js
@@ -1,133 +1,169 @@
-var modePrint = sessionStorage.getItem("mode_print");
-var time_kensin = sessionStorage.getItem("time_kensin");
-var user_reader = sessionStorage.getItem("user_reader");
+import * as Common from './Common/common_function.js'
 
-var form_mode_print = document.getElementById("get_print");
-// api url
-//http://192.168.200.218:8080/Webkensin/compackr/getSetting
-const api_url = "https://192.168.200.218/Webkensin/compackr/getSetting?key=0582668301&login_id=7&login_pw=7";
-var comment_1 = new Array();
-var comment_2 = new Array();
-var username = new Array();
+/*****  VIEW VARIABLE  *****/
+/* modal */
+const modal = document.getElementById("myModal");
 
-const user_name = document.getElementById("Combobox");
-const combobox_print_mode = document.getElementById("combobox_print_mode");
+/*****  DATA VARIABLE  *****/
+/* data setting */
+var dataSetting;
 
-var settingData = {
-	comment1: 0,
-	comment2: 0,
-	tanto: 0,
-	insatsu_mode: 0,
-}
-var settingDefault = settingData;
 
-// Calling that async function
-getapi(api_url);
 
-var data_comment;
-var data_username;
-// Defining async function
-async function getapi(url) {
-	// Storing response
-	const response = await fetch(url);
-	// Storing data in form of JSON
-	var data = await response.json();
-	data_comment = data.m_lstComment;
-	data_username = data.m_lstTantName;
-
-	for (let r of data_comment) {
-		if (r.code == 1) {
-			comment_1.push(r.name);
-		} else if (r.code == 2) {
-			comment_2.push(r.name);
-		}
-	}
-
-	for (let r of data_username) {
-		username.push(r.name);
-	}
-
-	populateClient();
-	setCommentCbb(1);
-	setCommentCbb(2);
-}
-
-//cancel button
-function canCel() {
-	window.location.href = "/menu.html";
-}
-
-//reset data in session when finish layout
-function resetTing() {
-	if (combobox_print_mode.value.localeCompare("画像印刷モード") == 0) {
-		sessionStorage.setItem("mode_print", "画像印刷モード");
-	} else {
-		sessionStorage.setItem("mode_print", "文字列印刷モード");
-	}
-
-	sessionStorage.setItem("user_reader", $("#Combobox option:selected").text());
-	history.back();
+/* 
+	GET DATA SETTING
+*/
+function getDataSetting() {
+	Common.setupModal("load", null, "データを保存しています。。。", null, null);
+	$.ajax({
+		url: "http://192.168.200.218:8080/Webkensin/compackr/getSetting?key=0582668301&login_id=" + sessionStorage.getItem('username') + "&login_pw=" + sessionStorage.getItem('password'),
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		success: function (result) {
+			dataSetting = JSON.parse(result);
+			setCommentCbb(1);
+			setCommentCbb(2);
+			setTantnameCbb();
+			setPrintModeCbb();
+			modal.style.display = "none";
+		},
+		error: function (jqXHR, exception) {
+			console.log(exception);
+		},
+		timeout: 10000
+	});
 }
 
 
-
-// get user
-function populateClient() {
-	if (user_reader == null) {
-	}
-	for (var i = 0; i < username.length; i++) {
-		var option = document.createElement("option");
-		option.classList.add("text")
-		if (user_reader == username[i]) {
-			option.selected = true;
-		}
-		option.text = username[i];
-		option.value = username[i];
-
-		user_name.add(option);
-	}
-
-	document.getElementById('Combobox').getElementsByTagName('option')[0].selected = 'selected'
-}
-
-
-// set comment
+/* 
+	SET DATA FOR COMMENT COMBOBOX
+	@param cbb
+*/
 function setCommentCbb(cbb) {
-	if (data_comment != null) {
-		for (let item of data_comment) {
-			var option = document.createElement("option");
-			option.classList.add("text")
-			option.text = item.name;
-			option.value = item.name;
-			document.getElementById("cbb_comment" + cbb).add(option);
+	if (dataSetting != null) {
+		if (dataSetting.m_lstComment != null) {
+			for (var i = 0; i < dataSetting.m_lstComment.length; i++) {
+				var option = document.createElement("option");
+				option.classList.add("text")
+				option.text = dataSetting.m_lstComment[i].name;
+				option.value = dataSetting.m_lstComment[i].code;
+				document.getElementById("cbb_comment" + cbb).add(option);
+
+				let commentSelected = cbb == 1 ? dataSetting.comment1 : dataSetting.comment2;
+				if (commentSelected == dataSetting.m_lstComment[i].code) {
+					option.selected = 'selected';
+				}
+			}
 		}
-		document.getElementById("cbb_comment" + cbb).getElementsByTagName('option')[0].selected = 'selected';
 	}
 }
 
-function setupModal(status, title, message) {
-	var modal = document.getElementById("myModal");
-	var titleModal = document.getElementsByClassName("title-modal")[0];
-	var buttonConfirm = document.getElementsByClassName("button-confirm")[0];
-	var closeButton = document.getElementsByClassName("modal-close-button")[0];
 
-
-	titleModal.innerHTML = title;
-
-	if (status == "load") {
-		//  titleModal.style.display = "none";
-		closeButton.style.display = "none";
-		//  buttonConfirm.style.display = "none";
-	}
-
-	if (modal.style.display == "none" || modal.style.display == "") {
-		modal.style.display = "block";
-	} else {
-		modal.style.display = "none";
-	}
-
-	// When the user clicks on <span> (x), close the modal
-	buttonConfirm.onclick = function () {
-		modal.style.display = "none";
+/* 
+	SET DATA FOR TANTNAME COMBOBOX
+*/
+function setTantnameCbb() {
+	if (dataSetting != null) {
+		if (dataSetting.m_lstTantName != null) {
+			for (var i = 0; i < dataSetting.m_lstTantName.length; i++) {
+				var option = document.createElement("option");
+				option.classList.add("text")
+				option.text = dataSetting.m_lstTantName[i].name;
+				option.value = dataSetting.m_lstTantName[i].code;
+				document.getElementById("cbb_tantname").add(option);
+				if (dataSetting.tancd == dataSetting.m_lstTantName[i].code) {
+					option.selected = true;
+				}
+			}
+		}
 	}
 }
+
+
+/* 
+	SET DATA FOR PRINT MODE COMBOBOX
+*/
+function setPrintModeCbb() {
+	if (dataSetting != null) {
+		if (dataSetting.prnt_mode == 0) {
+			document.getElementById("combobox_print_mode").value = "0";
+		} else {
+			document.getElementById("combobox_print_mode").value = "1";
+		}
+	}
+}
+
+
+/* 
+	PREPARE NEW DATA SETTING
+*/
+function prepareNewDataSetting() {
+	let comment1 = document.getElementById("cbb_comment1").value;
+	let comment2 = document.getElementById("cbb_comment2").value;
+	let tancd = document.getElementById("cbb_tantname").value;
+	let print_mode = document.getElementById("combobox_print_mode").value;
+	const newData = {
+		wrt_tancd: dataSetting.wrt_tancd,
+		tancd: tancd,
+		prnt_mode: print_mode,
+		comment1: comment1,
+		comment2: comment2,
+		m_nMode: dataSetting.wrt_tancd > 0 ? 1 : 0,
+		login_id: sessionStorage.getItem('username'),
+		login_pw: sessionStorage.getItem('password')
+	}
+	return newData;
+}
+
+
+/* 
+	SAVE DATA SETTING
+*/
+function saveDataSetting() {
+	Common.setupModal("load", null, "データを保存しています。。。", null, null);
+	$.ajax({
+		type: "POST",
+		data: JSON.stringify(prepareNewDataSetting()),
+		url: "http://192.168.200.218:8080/Webkensin/compackr/getSetting",
+		contentType: "application/json",
+		timeout: 100000,
+		success: function (response) {
+			console.log(response);
+			Common.setupModal("load", null, "データを保存しています。。。", null, null);
+		},
+		error: function (xmlhttprequest, textstatus, message) {
+			if (textstatus === "timeout") {
+				console.log("timeout")
+			} else {
+				console.log(textstatus)
+			}
+			// updateDialog("./images/gif/gif_fail.gif", "データ保存に失敗しました。", "red", true)
+			Common.setupModal("error", null, "データ保存に失敗しました。", "OK", null);
+		}
+	}).done(function (res) {
+		console.log('res', res);
+		Common.setupModal("success", null, "データ保存に成功しました。", "OK", null);
+	});
+}
+
+
+/* 
+	ONCLICK ACTION
+*/
+function onclickAction() {
+	document.getElementById("backPageButton").onclick = Common.backAction;
+	document.getElementById("saveButton").onclick = saveDataSetting;
+}
+
+
+/* 
+	ONLOAD ACTION
+*/
+function onLoadAction() {
+	getDataSetting();
+	onclickAction();
+}
+
+
+window.onload = onLoadAction;
