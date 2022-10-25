1add5700968685d47bee8ca463c82c325f714e9c
trung hieu <trunghieudinh224@gmail.com>
2022-10-25 13:15:48 +0700

    add event onclick for button

--- js/meter_reading_fillout.js
+++ js/meter_reading_fillout.js
@@ -1,6 +1,7 @@
 "use strict"
 import * as Common from './Common/common_function.js'
 import * as StringCS from './Constant/strings.js'
+import * as Mess from './Constant/message.js'
 
 const mUserData = JSON.parse(localStorage.getItem("UserData"));
 
@@ -12,21 +13,21 @@ const wrapMainForm = document.querySelector(".overlay .container-mainform .wrap-
 window.onload = setDefaultCollapse;
 
 function setDefaultCollapse() {
-    var mode = sessionStorage.getItem(StringCS.KINYUUMODE);
-    if (mode == 3) {
-        $('.collapseThree').collapse()
-    } else {
-        $('.collapseOne').collapse()
-    }
+	var mode = sessionStorage.getItem(StringCS.KINYUUMODE);
+	if (mode == 3) {
+		$('.collapseThree').collapse()
+	} else {
+		$('.collapseOne').collapse()
+	}
 }
 
-closeBtn.onclick = function(){
-    overlay.style.zIndex = "-1";
-    wrapMainForm.classList.remove("overlay-animate");
+closeBtn.onclick = function () {
+	overlay.style.zIndex = "-1";
+	wrapMainForm.classList.remove("overlay-animate");
 }
-detail.onclick = function(){
-    overlay.style.zIndex = "2";
-    wrapMainForm.classList.add("overlay-animate");
+detail.onclick = function () {
+	overlay.style.zIndex = "2";
+	wrapMainForm.classList.add("overlay-animate");
 }
 
 function isIOS() {
@@ -61,8 +62,19 @@ setAlignCombobox(isIOS());
 	SETUP OPTION MENU
 */
 function setOptionMenu() {
-    document.getElementById("menuOption").onclick = function() {Common.movePage('/menu_page.html')};
-    document.getElementById("settingOption").onclick = function() {Common.movePage('/setting_page.html')};
-    document.getElementById("logoutOption").onclick = function() {Common.movePage('logout')};
+	document.getElementById("menuOption").onclick = function () { Common.movePage('/menu_page.html') };
+	document.getElementById("settingOption").onclick = function () { Common.movePage('/setting_page.html') };
+	document.getElementById("logoutOption").onclick = function () { Common.movePage('logout') };
 }
 setOptionMenu();
+
+
+/**
+	* ONCLICK ACTION
+*/
+function onclickAction() {
+	document.getElementById("createPrintingFormButton").onclick = function () {
+		Common.setupModal("load", null, Mess.I00004, StringCS.OK);
+	};
+}
+onclickAction();
