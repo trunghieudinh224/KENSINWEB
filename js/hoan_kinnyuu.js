fd3e7e08a33c42e26192f5887dcdbac65f3e3d38
trung hieu <trunghieudinh224@gmail.com>
2022-12-06 09:09:54 +0700

    edit format code

--- js/hoan_kinnyuu.js
+++ js/hoan_kinnyuu.js
@@ -1,4 +1,4 @@
- import * as KensinKinyuu from './kensin_kinnyuu.js'
+import * as KensinKinyuu from './kensin_kinnyuu.js'
 
 
 export var m_bRes;
@@ -16,37 +16,37 @@ const unnecessary_btn = document.getElementById("unnecessary_btn");
 const no_btn = document.getElementById("no_btn");
 const good_btn = document.getElementById("good_btn");
 
-export var hoanString = KensinKinyuu.mKokfDat.mHoan.substring(0,8);
+export var hoanString = KensinKinyuu.mKokfDat.mHoan.substring(0, 8);
 var listHoanData = hoanString.split("");
 
 var pos_combobox = 0;
-const list_combobox = [combobox_1,combobox_2,combobox_3,combobox_4,combobox_5,combobox_6,combobox_7,combobox_8]; 
-for (let i = 0; i < list_combobox.length; i++) {
-    if(listHoanData[i] == "o"){
-        list_combobox[i].value = "0";
-    }
-    else if(listHoanData[i] == "-"){
-        list_combobox[i].value = "2";
-    }
-    else if(listHoanData[i] == "x"){
-        list_combobox[i].value = "1";
-    }
 
-  //  list_combobox[i].value = listHoanData[i];
-    
+var tenkenDelta = KensinKinyuu.sysfDat.mTenkenDelta;
+
+const list_combobox = [combobox_1, combobox_2, combobox_3, combobox_4, combobox_5, combobox_6, combobox_7, combobox_8];
+for (let i = 0; i < list_combobox.length; i++) {
+    switch (listHoanData[i]) {
+        case "o":
+            list_combobox[i].value = "0";
+            break;
+        case "-":
+            list_combobox[i].value = "2";
+            break;
+        case "x":
+            list_combobox[i].value = "1";
+            break;
+    }
 }
 
 list_combobox[pos_combobox].classList.add("combobox_select");
 
-var  tenkenDelta = 1;
-
-if(tenkenDelta == 1){
+if (tenkenDelta == 1) {
     var option = document.createElement("option");
     option.text = "△";
     option.value = "3";
     combobox_total.add(option);
 
-    list_combobox.forEach(item => {      
+    list_combobox.forEach(item => {
         item.add(option);
     });
 }
@@ -56,11 +56,11 @@ if(tenkenDelta == 1){
 var combo_choose;
 
 list_combobox.forEach(item => {
-    item.addEventListener("change",function(){
-        if(item.value == 1){
+    item.addEventListener("change", function () {
+        if (item.value == 1) {
             combobox_total.value = item.value;
-        }else{
-            setComboBoxTotal(item);           
+        } else {
+            setComboBoxTotal(item);
         }
     });
 });
@@ -68,52 +68,47 @@ list_combobox.forEach(item => {
 combobox_total.onchange = setAllComboBox;
 
 
-function setAllComboBox(){
-   
-        if(combobox_total.value != 1){
-                list_combobox.forEach(item => {
-                item.value = combobox_total.value;
-            });
-        }else{
-            var list = getHoan();
-            //console.log(list.length);
-            list.forEach(item => {
-               // console.log(item.code + " - "+ item.value + "\n");
-            });
+function setAllComboBox() {
 
-         //   isFirst = true;
-        } 
-        setHoanKinnyuu();
+    if (combobox_total.value != 1) {
+        list_combobox.forEach(item => {
+            item.value = combobox_total.value;
+        });
     }
+    setHoanKinnyuu();
+}
 
-function setComboBoxTotal(item){
-
+function setComboBoxTotal(item) {
     var isOne = true;
     var check = item.value;
     list_combobox.forEach(element => {
-        if(element.value != check){
+        if (element.value != check) {
             isOne = false;
         }
     });
 
-        if(isOne == true){
-            combobox_total.value = check;  
-        }
+    if (isOne == true) {
+        combobox_total.value = check;
+    }
 }
 
-function setDataforComboBoxSelect(value){
-    if(value == 0){
-        list_combobox[pos_combobox].value = 0;
-    }else if(value == 1){
-        combobox_total.value = 1;
-        list_combobox[pos_combobox].value = 1;
-    }else{
-        list_combobox[pos_combobox].value = 2;
+function setDataforComboBoxSelect(value) {
+    switch (value) {
+        case 0:
+            list_combobox[pos_combobox].value = 0;
+            break;
+        case 1:
+            combobox_total.value = 1;
+            list_combobox[pos_combobox].value = 1;
+            break;
+        default:
+            list_combobox[pos_combobox].value = 2;
+            break;
     }
     list_combobox[pos_combobox].classList.remove("combobox_select");
     setComboBoxTotal(list_combobox[pos_combobox]);
     pos_combobox++;
-    if(pos_combobox > 7){
+    if (pos_combobox > 7) {
         pos_combobox = 0;
     }
     list_combobox[pos_combobox].classList.add("combobox_select");
@@ -122,25 +117,30 @@ function setDataforComboBoxSelect(value){
 
 }
 
-function getHoan(){
+function getHoan() {
     var list_Value = new Array();
     var value = new Object();
     var result = "";
 
     for (let i = 0; i < list_combobox.length; i++) {
-        if(list_combobox[i].value == 0){
-            result =  "o";
-        }else if(list_combobox[i].value == 1){
-            result =  "x";
-        }else if(list_combobox[i].value == 2){
-            result =  "-";
-        }else{
-            result =  "*";
+        switch (list_combobox[i].value) {
+            case "o":
+                result = "o";
+                break;
+            case "-":
+                result = "x";
+                break;
+            case "x":
+                result = "-";
+                break;
+            default:
+                result = "*";
+                break;
         }
         value.code = i;
         value.value = result;
         list_Value.push(value);
-        
+
     }
     return list_Value;
 }
@@ -149,51 +149,51 @@ function getHoan(){
 
 const kenshin_data = document.getElementById("kenshin_data");
 
-function changeValue(){
+function changeValue() {
     console.log(kenshin_data.value);
 }
 
-function setHoanKinnyuu(){
+function setHoanKinnyuu() {
     hoanString = "";
     for (let i = 0; i < list_combobox.length; i++) {
         var hoanVal = list_combobox[i].options[list_combobox[i].selectedIndex].text;
         if (hoanVal == "ー") {
             hoanVal = "-";
         }
-       hoanString = hoanString + hoanVal;        
+        hoanString = hoanString + hoanVal;
     }
 }
 
-good_btn.addEventListener("click" ,function (){
-     setDataforComboBoxSelect(0);
-    
-}) ;
-unnecessary_btn.addEventListener("click" ,function (){
-     setDataforComboBoxSelect(2);
-    
-}) ;
-no_btn.addEventListener("click" ,function (){
-     setDataforComboBoxSelect(1);
-   
-}) ;
+good_btn.addEventListener("click", function () {
+    setDataforComboBoxSelect(0);
+
+});
+unnecessary_btn.addEventListener("click", function () {
+    setDataforComboBoxSelect(2);
+
+});
+no_btn.addEventListener("click", function () {
+    setDataforComboBoxSelect(1);
+
+});
 
 
 
 
-function getValueOfHoanItem(value){
-    if(value == 0){
+function getValueOfHoanItem(value) {
+    if (value == 0) {
         return 1;
-    }else if(value == 1){
+    } else if (value == 1) {
         return 2;
-    }else if(value == 2){
+    } else if (value == 2) {
         return 4;
     }
 }
 
-function setLawItem(){
+function setLawItem() {
     m_lLawItem = [];
     for (let i = 0; i < list_combobox.length; i++) {
-        var item =   new LawItemDat(i , getValueOfHoanItem(list_combobox[i].value));
-        m_lLawItem.push(item);     
+        var item = new LawItemDat(i, getValueOfHoanItem(list_combobox[i].value));
+        m_lLawItem.push(item);
     }
 }
