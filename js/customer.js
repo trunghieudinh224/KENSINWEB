ede838d263b9eb55702aefe27e4ac3a1b3ea40f6
trung hieu <trunghieudinh224@gmail.com>
2022-12-14 15:48:09 +0700

    edit jikko button

--- js/customer.js
+++ js/customer.js
@@ -58,8 +58,8 @@ function getInformation() {
     if (cusDat != null) {
         Common.setupModal("load", null, Mess.I00001, null, null);
         $.ajax({
-            url: StringCS.PR_HTTPS + StringCS.PR_ADDRESS + StringCS.PR_WEBNAME + StringCS.PR_READDATA + StringCS.PR_KEY + "&cusrec=" + cusDat.cusrec + "&login_id=" + sessionStorage.getItem(StringCS.PASSWORD) + "&login_pw=" + sessionStorage.getItem(StringCS.PASSWORD),
-            // url: StringCS.PR_HTTP + StringCS.PR_ADDRESS + StringCS.PR_PORT + StringCS.PR_WEBNAME + StringCS.PR_READDATA + StringCS.PR_KEY + "&cusrec=" + cusDat.cusrec + "&login_id=" + sessionStorage.getItem(StringCS.USERNAME) + "&login_pw=" + sessionStorage.getItem(StringCS.PASSWORD),
+            // url: StringCS.PR_HTTPS + StringCS.PR_ADDRESS + StringCS.PR_WEBNAME + StringCS.PR_READDATA + StringCS.PR_KEY + "&cusrec=" + cusDat.cusrec + "&htset=" + sessionStorage.getItem(StringCS.USERNAME) + "&login_id=" + sessionStorage.getItem(StringCS.PASSWORD) + "&login_pw=" + sessionStorage.getItem(StringCS.PASSWORD),
+            url: StringCS.PR_HTTP + StringCS.PR_ADDRESS + StringCS.PR_PORT + StringCS.PR_WEBNAME + StringCS.PR_READDATA + StringCS.PR_KEY + "&cusrec=" + cusDat.cusrec + "&htset=" + sessionStorage.getItem(StringCS.HTSETDATCODE) + "&login_id=" + sessionStorage.getItem(StringCS.USERNAME) + "&login_pw=" + sessionStorage.getItem(StringCS.PASSWORD),
             headers: {
                 'Content-Type': StringCS.PR_CONTENT_TYPE
             },
@@ -122,6 +122,14 @@ function setInformation() {
             document.getElementById("memo-area").style.display == "none") {
             document.getElementsByClassName("card-2")[0].style.display = "none";
         }
+    }
+
+
+    var jikkoBtn = document.getElementById("jikkoButton");
+    if (cusDetailData.mKokfDat.mNoKensin == 0) {
+        jikkoBtn.disabled = true;
+    } else {
+        jikkoBtn.disabled = false;
     }
 }
 
