f11c8af2b84524f5f074cc2813cce43c7a5fd441
trung hieu <trunghieudinh224@gmail.com>
2022-12-16 08:10:13 +0700

    handle and show data list

--- js/product_search_page.js
+++ js/product_search_page.js
@@ -64,11 +64,11 @@ function getData() {
 			'Content-Type': StringCS.PR_CONTENT_TYPE
 		},
 		success: function (result) {
-			
-			const data = JSON.parse(result);
-			shofDatList = data.m_MapShofDat;
-			busfDatList = data.m_lstBufDat;
 			modal.style.display = "none";
+			const data = JSON.parse(result);
+			busfDatList = data.m_lstBufDat;
+			shofDatList = new Map(Object.entries(data.m_MapShofDat))
+			setData();
 		},
 		error: function (jqXHR, exception) {
 			console.log(exception);
@@ -79,10 +79,138 @@ function getData() {
 }
 
 
+/**
+   * SET DATA
+*/
 function setData() {
-	for (var i = 0; i < shofDatList.length; i++) {
+	var area = document.getElementById("accordion");
+	var previousId = "";
+	var idx = 0;
+	shofDatList.forEach((value, key) => {
+		// console.log("values: " + values + " / " + "key: " + keys + "\n");
+		var collapse;
+		collapse = createCollapseTitle(getValueCollapse(key), idx);
 
+		if (value.length > 1) {
+			const collapseItem = document.createElement("div");
+			collapseItem.className += "collapse ";
+			collapseItem.className += "collapseItem" + String(idx);
+			collapseItem.id = "collapseItem" + String(idx);
+			collapseItem.setAttribute("data-parent", "#accordion");
+
+			const cardBody = document.createElement("div");
+			cardBody.className += "card-body";
+
+			const tableContainer = document.createElement("div");
+			tableContainer.className += "table-container";
+
+			const table = document.createElement("table");
+			table.className += "result-detail";
+			table.id += "detail" + String(idx);
+
+			collapse.appendChild(collapseItem);
+			collapseItem.appendChild(cardBody);
+			cardBody.appendChild(tableContainer);
+			tableContainer.appendChild(table);
+			for (var i = 0; i < value.length; i++) {
+				const tr = document.createElement("tr");
+				tr.className += "h-40 ";
+				tr.className += "hover ";
+				tr.className += "detailItem" + String(idx);
+				tr.id = "detailItem" + String(idx) + "_" + String(i);
+
+				const td = document.createElement("td");
+				
+				const span = document.createElement("span");
+				span.className += "text ";
+				span.className += "pd-l-20";
+				var itemDetail = String(value[i].mShono);
+				span.appendChild(document.createTextNode(itemDetail.length > 1 ? itemDetail : ("0" + itemDetail)));
+
+				tr.appendChild(td);
+				td.appendChild(span);
+				table.appendChild(tr);
+				tr.onclick = function () {
+					const shofdat = Object.assign({}, value[this.rowIndex]);
+					sessionStorage.setItem(StringCS.SHOFDATITEM, JSON.stringify(shofdat));
+					var id = (this.parentElement).parentElement.parentElement.parentElement.id;
+					const busfDat = Object.assign({}, busfDatList[parseInt(id.substring(id.length-1, id.length))]);
+					sessionStorage.setItem(StringCS.BUSFDATITEM, JSON.stringify(busfDat));
+					Common.movePage('/purchase_page.html');
+				};
+			}
+		} else {
+			collapse.onclick = function () {
+				const shofdat = Object.assign({}, value[0]);
+				sessionStorage.setItem(StringCS.SHOFDATITEM, JSON.stringify(shofdat));
+				var id = collapse.id;
+				const busfDat = Object.assign({}, busfDatList[parseInt(id.substring(id.length-1, id.length))]);
+				sessionStorage.setItem(StringCS.BUSFDATITEM, JSON.stringify(busfDat));
+				Common.movePage('/purchase_page.html');
+			};
+		}
+
+		if (idx == 0) {
+			area.appendChild(collapse);
+			previousId = collapse.id;
+		} else {
+			const preElement = document.getElementById(previousId);
+			preElement.after(collapse);
+			previousId = collapse.id;
+		}
+		
+		idx++;
+	})
+}
+
+
+/**
+   * GET VALUE COLLAPSE
+   * @param hino     [STRING]
+*/
+function getValueCollapse(hino) {
+	var result = hino + " ";
+	for (var i = 0; i < busfDatList.length; i++) {
+		if (busfDatList[i].mHinno == parseInt(hino)) {
+			result += busfDatList[i].mName;
+			break;
+		}
 	}
+	return result;
+}
+
+
+/**
+   * CREATE COLLAPSE TITLE
+   * @param text     [STRING]
+   * @param idx     [INT]
+*/
+function createCollapseTitle(text, idx) {
+	const card = document.createElement("div");
+	card.className += "card ";
+	card.className += "card-" + String(idx);
+	card.id = "card" + String(idx);
+
+	const cardHeader = document.createElement("div");
+	cardHeader.className += "card-header";
+
+	const cardLink = document.createElement("span");
+	cardLink.className += "card-link";
+	cardLink.setAttribute("data-toggle", "collapse");
+	cardLink.setAttribute("href", "#collapseItem" + String(idx));
+
+	const itemCollapse = document.createElement("div");
+	itemCollapse.className += "item-collapse";
+
+	const itemCollapseText = document.createElement("span");
+	itemCollapseText.className += "text";
+	itemCollapseText.appendChild(document.createTextNode(Other.cutStringSpace(text)));
+
+	card.appendChild(cardHeader);
+	cardHeader.appendChild(cardLink);
+	cardLink.appendChild(itemCollapse);
+	itemCollapse.appendChild(itemCollapseText);
+	return card;
 }
 
 
