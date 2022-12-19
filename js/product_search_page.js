88e58b9a7ad69797dc12e5dfc4407efba28553f2
trung hieu <trunghieudinh224@gmail.com>
2022-12-19 10:14:07 +0700

    edit product_search_page

--- js/product_search_page.js
+++ js/product_search_page.js
@@ -14,6 +14,12 @@ const modal = document.getElementById("myModal");
 var shofDatList;
 /* busfDatList */
 var busfDatList;
+/* listBustCho */
+var listBustCho;
+/* list bustnyu */
+var listBustNyu;
+/* User Data */
+const mUserData = JSON.parse(sessionStorage.getItem(StringCS.USERDATA));
 
 /* 
 	SETUP OPTION MENU
@@ -53,7 +59,6 @@ function setOnClickItem() {
    * SEARCH CUSTOMER
 */
 function getData() {
-
 	Common.setupModal("load", null, Mess.I00001, null, null);
 	$.ajax({
 		// url: StringCS.PR_HTTPS + StringCS.PR_ADDRESS + StringCS.PR_WEBNAME + StringCS.PR_EARNING + StringCS.PR_KEY +
@@ -68,7 +73,13 @@ function getData() {
 			const data = JSON.parse(result);
 			busfDatList = data.m_lstBufDat;
 			shofDatList = new Map(Object.entries(data.m_MapShofDat))
-			setData();
+			listBustCho = prepareUriageData(3);
+			listBustNyu = prepareUriageData(1);
+			sessionStorage.setItem(StringCS.BUSFDATLIST, JSON.stringify(busfDatList));
+			sessionStorage.setItem(StringCS.SHOFDATLIST, JSON.stringify(Array.from(shofDatList.entries())));
+			sessionStorage.setItem(StringCS.LISTBUSTCHO, JSON.stringify(listBustCho));
+			sessionStorage.setItem(StringCS.LISTBUSTNYU, JSON.stringify(listBustNyu));
+			setData(shofDatList, busfDatList);
 		},
 		error: function (jqXHR, exception) {
 			console.log(exception);
@@ -79,88 +90,100 @@ function getData() {
 }
 
 
+function setCusData() {
+	document.getElementById("codeVal").innerHTML = Other.cutStringSpace(mUserData.mKokfDat.mCusCode);
+	document.getElementById("nameVal").innerHTML = Other.cutStringSpace(mUserData.mKokfDat.mName);
+}
+
+
 /**
    * SET DATA
 */
-function setData() {
+function setData(shofDatList, busfDatList) {
+	var dataMessage = document.getElementById("data-messages");
 	var area = document.getElementById("accordion");
 	var previousId = "";
 	var idx = 0;
-	shofDatList.forEach((value, key) => {
-		// console.log("values: " + values + " / " + "key: " + keys + "\n");
-		var collapse;
-		collapse = createCollapseTitle(getValueCollapse(key), idx);
-
-		if (value.length > 1) {
-			const collapseItem = document.createElement("div");
-			collapseItem.className += "collapse ";
-			collapseItem.className += "collapseItem" + String(idx);
-			collapseItem.id = "collapseItem" + String(idx);
-			collapseItem.setAttribute("data-parent", "#accordion");
-
-			const cardBody = document.createElement("div");
-			cardBody.className += "card-body";
-
-			const tableContainer = document.createElement("div");
-			tableContainer.className += "table-container";
-
-			const table = document.createElement("table");
-			table.className += "result-detail";
-			table.id += "detail" + String(idx);
-
-			collapse.appendChild(collapseItem);
-			collapseItem.appendChild(cardBody);
-			cardBody.appendChild(tableContainer);
-			tableContainer.appendChild(table);
-			for (var i = 0; i < value.length; i++) {
-				const tr = document.createElement("tr");
-				tr.className += "h-40 ";
-				tr.className += "hover ";
-				tr.className += "detailItem" + String(idx);
-				tr.id = "detailItem" + String(idx) + "_" + String(i);
-
-				const td = document.createElement("td");
-				
-				const span = document.createElement("span");
-				span.className += "text ";
-				span.className += "pd-l-20";
-				var itemDetail = String(value[i].mShono);
-				span.appendChild(document.createTextNode(itemDetail.length > 1 ? itemDetail : ("0" + itemDetail)));
-
-				tr.appendChild(td);
-				td.appendChild(span);
-				table.appendChild(tr);
-				tr.onclick = function () {
-					const shofdat = Object.assign({}, value[this.rowIndex]);
+	if (shofDatList.size > 0 && busfDatList.length > 0) {
+		dataMessage.style.display = "none";
+		shofDatList.forEach((value, key) => {
+			var collapse;
+			collapse = createCollapseTitle(getValueCollapse(key), idx, value.length);
+	
+			if (value.length > 1) {
+				const collapseItem = document.createElement("div");
+				collapseItem.className += "collapse ";
+				collapseItem.className += "collapseItem" + String(idx);
+				collapseItem.id = "collapseItem" + String(idx);
+				collapseItem.setAttribute("data-parent", "#accordion");
+	
+				const cardBody = document.createElement("div");
+				cardBody.className += "card-body";
+	
+				const tableContainer = document.createElement("div");
+				tableContainer.className += "table-container";
+	
+				const table = document.createElement("table");
+				table.className += "result-detail";
+				table.id += "detail" + String(idx);
+	
+				collapse.appendChild(collapseItem);
+				collapseItem.appendChild(cardBody);
+				cardBody.appendChild(tableContainer);
+				tableContainer.appendChild(table);
+				for (var i = 0; i < value.length; i++) {
+					const tr = document.createElement("tr");
+					tr.className += "h-40 ";
+					tr.className += "hover ";
+					tr.className += "detailItem" + String(idx);
+					tr.id = "detailItem" + String(idx) + "_" + String(i);
+	
+					const td = document.createElement("td");
+	
+					const span = document.createElement("span");
+					span.className += "text ";
+					span.className += "pd-l-20";
+					var itemDetail = String(value[i].mShono);
+					span.appendChild(document.createTextNode(itemDetail.length > 1 ? itemDetail : ("0" + itemDetail)));
+	
+					tr.appendChild(td);
+					td.appendChild(span);
+					table.appendChild(tr);
+					tr.onclick = function () {
+						const shofdat = Object.assign({}, value[this.rowIndex]);
+						sessionStorage.setItem(StringCS.SHOFDATITEM, JSON.stringify(shofdat));
+						var id = (this.parentElement).parentElement.parentElement.parentElement.id;
+						const busfDat = Object.assign({}, busfDatList[parseInt(id.substring(id.length - 1, id.length))]);
+						sessionStorage.setItem(StringCS.BUSFDATITEM, JSON.stringify(busfDat));
+						Common.movePage('/purchase_page.html');
+					};
+				}
+			} else {
+				collapse.onclick = function () {
+					const shofdat = Object.assign({}, value[0]);
 					sessionStorage.setItem(StringCS.SHOFDATITEM, JSON.stringify(shofdat));
-					var id = (this.parentElement).parentElement.parentElement.parentElement.id;
-					const busfDat = Object.assign({}, busfDatList[parseInt(id.substring(id.length-1, id.length))]);
+					var id = collapse.id;
+					const busfDat = Object.assign({}, busfDatList[parseInt(id.substring(id.length - 1, id.length))]);
 					sessionStorage.setItem(StringCS.BUSFDATITEM, JSON.stringify(busfDat));
 					Common.movePage('/purchase_page.html');
 				};
 			}
-		} else {
-			collapse.onclick = function () {
-				const shofdat = Object.assign({}, value[0]);
-				sessionStorage.setItem(StringCS.SHOFDATITEM, JSON.stringify(shofdat));
-				var id = collapse.id;
-				const busfDat = Object.assign({}, busfDatList[parseInt(id.substring(id.length-1, id.length))]);
-				sessionStorage.setItem(StringCS.BUSFDATITEM, JSON.stringify(busfDat));
-				Common.movePage('/purchase_page.html');
-			};
-		}
-
-		if (idx == 0) {
-			area.appendChild(collapse);
-			previousId = collapse.id;
-		} else {
-			const preElement = document.getElementById(previousId);
-			preElement.after(collapse);
-			previousId = collapse.id;
-		}
-		
-		idx++;
-	})
+	
+			if (idx == 0) {
+				area.appendChild(collapse);
+				previousId = collapse.id;
+			} else {
+				const preElement = document.getElementById(previousId);
+				preElement.after(collapse);
+				previousId = collapse.id;
+			}
+	
+			idx++;
+		})
+	} else {
+		dataMessage.innerText = Mess.E00001;
+		dataMessage.style.display = "block";
+	}
 }
 
 
@@ -185,7 +208,7 @@ function getValueCollapse(hino) {
    * @param text     [STRING]
    * @param idx     [INT]
 */
-function createCollapseTitle(text, idx) {
+function createCollapseTitle(text, idx, ken) {
 	const card = document.createElement("div");
 	card.className += "card ";
 	card.className += "card-" + String(idx);
@@ -200,17 +223,103 @@ function createCollapseTitle(text, idx) {
 	cardLink.setAttribute("href", "#collapseItem" + String(idx));
 
 	const itemCollapse = document.createElement("div");
-	itemCollapse.className += "item-collapse";
+	itemCollapse.className += "item-collapse ";
+	itemCollapse.className += "w-100p";
+
+	const divText = document.createElement("div");
+	divText.className += "w-75p ";
+	divText.className += "float-l";
 
 	const itemCollapseText = document.createElement("span");
 	itemCollapseText.className += "text";
 	itemCollapseText.appendChild(document.createTextNode(Other.cutStringSpace(text)));
 
+	const divCount = document.createElement("div");
+	divCount.className += "w-25p ";
+	divCount.className += "float-r ";
+	divCount.className += "ta-r";
+
+	const itemCollapseCount = document.createElement("span");
+	itemCollapseCount.className += "text";
+	if (ken > 1) {
+		itemCollapseCount.appendChild(document.createTextNode("(" + String(ken)+ "件" + ")"));
+	}
+
 	card.appendChild(cardHeader);
 	cardHeader.appendChild(cardLink);
 	cardLink.appendChild(itemCollapse);
-	itemCollapse.appendChild(itemCollapseText);
+	divText.appendChild(itemCollapseText);
+	itemCollapse.appendChild(divText);
+	divCount.appendChild(itemCollapseCount);
+	divText.after(divCount);
 	return card;
+}
+
+
+/**
+   * SEARCH PRODUCT
+   * @param value     [STRING]
+*/
+function searchProduct(value) {
+	var listView = document.getElementById("accordion");
+	setData(shofDatList, busfDatList);
+	if (value.length > 0) {
+		var listItem = document.getElementsByClassName("card");
+		var shofDatListTemp = new Map(shofDatList);
+		for (var i = 0; i < listItem.length; i++) {
+			const name = listItem[i].childNodes;
+			var itemVal = name[0].childNodes[0].childNodes[0].childNodes[0].textContent;
+			var codeVal = itemVal.split(" ")[0];
+
+			if (Other.cutStringSpace(itemVal).includes(Other.cutStringSpace(value))) {
+				console.log(itemVal);
+			} else {
+				shofDatListTemp.delete(codeVal);
+			}
+		}
+
+		while (listView.hasChildNodes()) {
+			listView.removeChild(listView.firstChild);
+		}
+		if (shofDatListTemp.size != shofDatList.size) {
+			setData(shofDatListTemp, busfDatList);
+			listView.scrollTop = 0;
+		}
+	} else {
+		setData(shofDatList, busfDatList);
+		listView.scrollTop = 0;
+	}
+}
+
+
+function setLayoutDefault() {
+	const value = Other.nullToString(sessionStorage.getItem(StringCS.PRODUCTSTRING));
+	if (value != "") {
+		searchProduct(value);
+	}
+}
+
+
+function prepareUriageData(kind) {
+	var list;
+	for (var i = 0; i < busfDatList.length; i++) {
+		if (busfDatList.mKind == kind) {
+			list.push(busfDatList[i]);
+		}
+	}
+	return list;
+}
+
+
+/**
+   * ONCLICK ACTION
+*/
+function onclickAction() {
+	document.getElementById("kensakuButton").onclick = function () {
+		const val = Other.cutStringSpace(document.getElementById("searchString").value);
+		sessionStorage.setItem(StringCS.PRODUCTSTRING, val);
+		searchProduct(val);
+	};
 }
 
 
@@ -219,8 +328,23 @@ function createCollapseTitle(text, idx) {
 */
 function onLoadAction() {
 	setOptionMenu();
+	onclickAction();
+	setCusData();
+	if (Other.nullToString(sessionStorage.getItem(StringCS.BUSFDATLIST)) != "" && Other.nullToString(sessionStorage.getItem(StringCS.SHOFDATLIST)) != "") {
+		busfDatList = JSON.parse(sessionStorage.getItem(StringCS.BUSFDATLIST));
+		shofDatList = new Map(JSON.parse(sessionStorage.getItem(StringCS.SHOFDATLIST)));
+		const searchString = Other.nullToString(sessionStorage.getItem(StringCS.PRODUCTSTRING));
+		if (searchString.length > 0) {
+			document.getElementById("searchString").value = searchString
+			searchProduct(searchString);
+		} else {
+			setData(shofDatList, busfDatList);
+		}
+	} else {
+		getData();
+	}
 	setOnClickItem();
-	getData();
+	setLayoutDefault();
 }
 
 
