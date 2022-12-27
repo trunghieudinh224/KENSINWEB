350c7c969cb7cf3976f97295a04c74582e60acda
trung hieu <trunghieudinh224@gmail.com>
2022-12-27 12:59:21 +0700

    change api link

--- js/product_search.js
+++ js/product_search.js
@@ -60,15 +60,11 @@ function setOnClickItem() {
 */
 function getData() {
 	Common.setupModal("load", null, Mess.I00001, null, null);
-	var str = StringCS.PR_HTTP + StringCS.PR_ADDRESS + StringCS.PR_PORT + StringCS.PR_WEBNAME + StringCS.PR_EARNING + StringCS.PR_KEY +
-			"&login_id=" + sessionStorage.getItem(StringCS.USERNAME) +
-			"&login_pw=" + sessionStorage.getItem(StringCS.PASSWORD);
 	$.ajax({
 		url: StringCS.PR_HTTPS + StringCS.PR_ADDRESS + StringCS.PR_WEBNAME + StringCS.PR_EARNING + StringCS.PR_KEY +
 		// url: StringCS.PR_HTTP + StringCS.PR_ADDRESS + StringCS.PR_PORT + StringCS.PR_WEBNAME + StringCS.PR_EARNING + StringCS.PR_KEY +
 			"&login_id=" + sessionStorage.getItem(StringCS.USERNAME) +
 			"&login_pw=" + sessionStorage.getItem(StringCS.PASSWORD),
-		url: str,
 		headers: {
 			'Content-Type': StringCS.PR_CONTENT_TYPE
 		},
