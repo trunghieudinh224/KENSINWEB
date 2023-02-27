fede5d76c33c3a250bedfa96349a97c1598ad8c9
trung hieu <trunghieudinh224@gmail.com>
2023-02-27 15:43:44 +0700

    test function

--- js/meter_reading_setting.js
+++ js/meter_reading_setting.js
@@ -87,22 +87,11 @@ function onLoadAction() {
 
 window.onload = onLoadAction;
 
-
-window.addEventListener('popstate', function(event) {
-    // The popstate event is fired each time when the current history entry changes.
-
-    var r = confirm("You pressed a Back button! Are you sure?!");
-
-    if (r == true) {
-        // Call Back button programmatically as per user confirmation.
-        history.back();
-        // Uncomment below line to redirect to the previous page instead.
-        // window.location = document.referrer // Note: IE11 is not supporting this.
-    } else {
-        // Stay on the current page.
-        history.pushState(null, null, window.location.pathname);
-    }
-
-    history.pushState(null, null, window.location.pathname);
-
-}, false);
+$(window).bind('beforeunload', function(){
+	return true
+  });
+  
+  function myFunction(){
+	   // Write your business logic here
+	   alert('Bye');
+  }
