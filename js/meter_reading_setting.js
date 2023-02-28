53baa1af6372a86b85187b157dee1aca108fdc44
trung hieu <trunghieudinh224@gmail.com>
2023-02-28 09:22:39 +0700

    test function

--- js/meter_reading_setting.js
+++ js/meter_reading_setting.js
@@ -85,4 +85,15 @@ function onLoadAction() {
 }
 
 
+history.pushState(null, document.title, location.href);
+window.addEventListener('popstate', function (event)
+{
+    const leavePage = confirm("you want to go ahead ?");
+    if (leavePage) {
+        history.back(); 
+    } else {
+        history.pushState(null, document.title, location.href);
+    }  
+});
+
 window.onload = onLoadAction;
